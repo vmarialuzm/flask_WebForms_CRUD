f13a362cd254f1360eef889c31563808931fe2f1
vmarialuzm <vmarialuzm@gmail.com>
2023-03-27 17:30:53 -0500

    CRUD

---
+++ models/db/alumno.py
@@ -0,0 +1,8 @@
+from app import db
+
+class Alumno(db.Model):
+    id=db.Column(db.Integer,autoincrement=True,primary_key=True)
+    nombre=db.Column(db.String(100),primary_key=True)
+    apellido=db.Column(db.String(100),primary_key=True)
+    idaula=db.Column(db.Integer,db.ForeignKey('salon.id'),primary_key=True)
+
